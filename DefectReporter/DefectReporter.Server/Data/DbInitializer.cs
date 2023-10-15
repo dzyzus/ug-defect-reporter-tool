d7be335e91477cea3accc1b8a0085d0f725b1391
dzyzus <dzyzusek@gmail.com>
2023-10-15 11:55:40 +0200

    Project init

---
+++ DefectReporter/DefectReporter.Server/Data/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace DefectReporter.Server.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(DefectReporterContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
---
+++ DefectReporter/DefectReporter.Server/Data/DefectReporterContext.cs
@@ -0,0 +1,24 @@
+﻿namespace DefectReporter.Server.Data
+{
+    #region Usings
+
+    using DefectReporter.Shared.Models;
+    using Microsoft.EntityFrameworkCore;
+    
+    #endregion
+
+    public class DefectReporterContext : DbContext
+    {
+        /// <summary>
+        /// The defect reporter context constructor.
+        /// </summary>
+        /// <param name="options">
+        /// The db context options.
+        /// </param>
+        public DefectReporterContext(DbContextOptions<DefectReporterContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Defect> Defects { get; set; }
+    }
+}
