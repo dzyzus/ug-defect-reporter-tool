18fb3aa286f23e1125be71854412d2b5b3b05eb0
dzyzus <dzyzusek@gmail.com>
2024-01-03 18:16:29 +0100

    added charts

--- DefectReporter/Client/wwwroot/scripts/chart.js
+++ DefectReporter/Client/wwwroot/scripts/chart.js
@@ -11,4 +11,64 @@
             }]
         }
     });
+};
+
+function createPieChartFourColors(canvasId, labels, data) {
+    var ctx = document.getElementById(canvasId).getContext('2d');
+
+    var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: data,
+                backgroundColor: ['#8A2BE2', '#48D1CC', '#32CD32', '#A52A2A']
+            }]
+        }
+    });
+};
+
+function createBarChart(canvasId, labels, data) {
+    var canvas = document.getElementById(canvasId);
+
+    if (canvas) {
+        var ctx = canvas.getContext('2d');
+
+        var barChartOptions = {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Frequency of defects',
+                    data: data,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'day',
+                            parser: 'YYYY-MM-DD',
+                            tooltipFormat: 'll',
+                            displayFormats: {
+                                day: 'YYYY-MM-DD'
+                            }
+                        }
+                    },
+                    y: {
+                        beginAtZero: true,
+                        stepSize: 1
+                    }
+                }
+            }
+        };
+
+        var barChart = new Chart(ctx, barChartOptions);
+    } else {
+        console.error("Canvas element with id '" + canvasId + "' not found.");
+    }
 }
--- DefectReporter/Server/Data/Application/DefectReporterContext.cs
+++ DefectReporter/Server/Data/Application/DefectReporterContext.cs
@@ -225,6 +225,319 @@
                     TypeOfTest = TypeOfTest.Smoke,
                     FeatureId = 3,
                     Component = ComponentEnum.Feature
+                },
+                new
+                {
+                    Id = 8,
+                    TestName = "Check the correctness of data during account registration",
+                    TestCase = "1. Open the account registration form\n2. Provide correct data\n3. Check if the account registration with correct data will be completed",
+                    IsAutomated = true,
+                    TypeOfTest = TypeOfTest.Functional,
+                    FeatureId = 2,
+                    Component = ComponentEnum.Authentication
+                },
+                new
+                {
+                    Id = 9,
+                    TestName = "Check the account registration process with incorrect data",
+                    TestCase = "1. Open the account registration form\n2. Provide incorrect data\n3. Check if the account registration with incorrect data will be interrupted",
+                    IsAutomated = true,
+                    TypeOfTest = TypeOfTest.Security,
+                    FeatureId = 2,
+                    Component = ComponentEnum.Authentication
+                },
+                new
+                {
+                    Id = 10,
+                    TestName = "Check the login process with correct data",
+                    TestCase = "1. Open the login form\n2. Provide correct data\n3. Check if the login process will be completed",
+                    IsAutomated = true,
+                    TypeOfTest = TypeOfTest.Functional,
+                    FeatureId = 1,
+                    Component = ComponentEnum.Authentication
+                },
+                new
+                {
+                    Id = 11,
+                    TestName = "Check the login process with incorrect data",
+                    TestCase = "1. Open the login form\n2. Provide incorrect data\n3. Check if the login process will be interrupted",
+                    IsAutomated = true,
+                    TypeOfTest = TypeOfTest.Security,
+                    FeatureId = 1,
+                    Component = ComponentEnum.Authentication
+                },
+                new
+                {
+                    Id = 12,
+                    TestName = "Check if the URL will be opened properly",
+                    TestCase = "1. Provide URL\n2. Check if the provided URL will be opened properly",
+                    IsAutomated = true,
+                    TypeOfTest = TypeOfTest.Smoke,
+                    FeatureId = 1,
+                    Component = ComponentEnum.Authentication
+                },
+                new
+                {
+                    Id = 13,
+                    TestName = "Check if the URL will be opened properly",
+                    TestCase = "1. Provide URL\n2. Check if the provided URL will be opened properly",
+                    IsAutomated = true,
+                    TypeOfTest = TypeOfTest.Smoke,
+                    FeatureId = 2,
+                    Component = ComponentEnum.Authentication
+                },
+                new
+                {
+                    Id = 14,
+                    TestName = "Check if the URL will be opened properly",
+                    TestCase = "1. Provide URL\n2. Check if the provided URL will be opened properly",
+                    IsAutomated = true,
+                    TypeOfTest = TypeOfTest.Smoke,
+                    FeatureId = 3,
+                    Component = ComponentEnum.Feature
+                }
+                );
+
+            modelBuilder.Entity<Defect>().HasData(
+                new
+                {
+                    Id = 1,
+                    Title = "[AUTH] Can't confirm my email",
+                    Description = "1. Register new account\n2. Notice that the email can not be confirmed",
+                    ReleaseId = 1,
+                    IsRegression = false,
+                    IsFixed = false,
+                    Created = DateTime.Now.AddDays(-10),
+                    Status = DefectStatusEnum.Open,
+                    Component = ComponentEnum.Authentication,
+                    SoftwareId = 1,
+                    TestId = 1,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                },
+                new
+                {
+                    Id = 2,
+                    Title = "[AUTH] The registration button from login view is not working",
+                    Description = "1. Go to login view\n2. Try go to register view from login view",
+                    ReleaseId = 1,
+                    IsRegression = true,
+                    Created = DateTime.Now.AddDays(-9),
+                    IsFixed = false,
+                    Status = DefectStatusEnum.Open,
+                    Component = ComponentEnum.Authentication,
+                    SoftwareId = 2,
+                    TestId = 1,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                },
+                new
+                {
+                    Id = 3,
+                    Title = "[AUTH] The registration button from login view is not working",
+                    Description = "1. Go to login view\n2. Try go to register view from login view",
+                    ReleaseId = 1,
+                    IsRegression = true,
+                    Created = DateTime.Now.AddDays(-9),
+                    IsFixed = true,
+                    Status = DefectStatusEnum.Rejected,
+                    Component = ComponentEnum.Authentication,
+                    SoftwareId = 2,
+                    TestId = 1,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                },
+                new
+                {
+                    Id = 4,
+                    Title = "[AUTH] Password reset not working",
+                    Description = "1. Click on 'Forgot Password'\n2. Provide the email\n3. Check if the password reset link is sent",
+                    ReleaseId = 1,
+                    IsRegression = false,
+                    IsFixed = false,
+                    Created = DateTime.Now.AddDays(-8),
+                    Status = DefectStatusEnum.Open,
+                    Component = ComponentEnum.Authentication,
+                    SoftwareId = 3,
+                    TestId = 2,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                },
+                new
+                {
+                    Id = 5,
+                    Title = "[DATA] Unable to save data changes",
+                    Description = "1. Make changes to data\n2. Save the changes\n3. Check if the changes are saved properly",
+                    ReleaseId = 1,
+                    IsRegression = false,
+                    IsFixed = false,
+                    Created = DateTime.Now.AddDays(-7),
+                    Status = DefectStatusEnum.Open,
+                    Component = ComponentEnum.Data,
+                    SoftwareId = 4,
+                    TestId = 3,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                },
+                new
+                {
+                    Id = 6,
+                    Title = "[FEATURE] Missing button for new feature",
+                    Description = "1. Open the feature creation screen\n2. Check if there is a button to create a new feature",
+                    ReleaseId = 2,
+                    IsRegression = false,
+                    IsFixed = false,
+                    Created = DateTime.Now.AddDays(-6),
+                    Status = DefectStatusEnum.Open,
+                    Component = ComponentEnum.Feature,
+                    SoftwareId = 5,
+                    TestId = 4,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                },
+                new
+                {
+                    Id = 7,
+                    Title = "[DATA] Unable to delete records",
+                    Description = "1. Attempt to delete a record\n2. Check if the record is successfully deleted",
+                    ReleaseId = 2,
+                    IsRegression = false,
+                    IsFixed = false,
+                    Created = DateTime.Now.AddDays(-5),
+                    Status = DefectStatusEnum.Open,
+                    Component = ComponentEnum.Data,
+                    SoftwareId = 6,
+                    TestId = 5,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                },
+                new
+                {
+                    Id = 8,
+                    Title = "[FEATURE] Incorrect sorting in feature list",
+                    Description = "1. Open the feature list\n2. Check if the features are sorted correctly",
+                    ReleaseId = 2,
+                    IsRegression = false,
+                    IsFixed = false,
+                    Created = DateTime.Now.AddDays(-4),
+                    Status = DefectStatusEnum.Open,
+                    Component = ComponentEnum.Feature,
+                    SoftwareId = 7,
+                    TestId = 6,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                },
+                new
+                {
+                    Id = 9,
+                    Title = "[DATA] Data integrity issue",
+                    Description = "1. Perform multiple data operations\n2. Check if the data integrity is maintained",
+                    ReleaseId = 2,
+                    IsRegression = false,
+                    IsFixed = false,
+                    Created = DateTime.Now.AddDays(-3),
+                    Status = DefectStatusEnum.Open,
+                    Component = ComponentEnum.Data,
+                    SoftwareId = 8,
+                    TestId = 7,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                },
+                new
+                {
+                    Id = 10,
+                    Title = "[FEATURE] Missing feature details",
+                    Description = "1. Open the details of a feature\n2. Check if all details are displayed",
+                    ReleaseId = 2,
+                    IsRegression = false,
+                    IsFixed = false,
+                    Created = DateTime.Now.AddDays(-2),
+                    Status = DefectStatusEnum.Open,
+                    Component = ComponentEnum.Feature,
+                    SoftwareId = 9,
+                    TestId = 8,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                },
+                new
+                {
+                    Id = 11,
+                    Title = "[FEATURE] Incorrect display of feature status",
+                    Description = "1. Open the feature details\n2. Check if the status of the feature is displayed correctly",
+                    ReleaseId = 3,
+                    IsRegression = false,
+                    IsFixed = true,
+                    CompletedReason = DefectCompletedReasonEnum.Fixed,
+                    FixedOnVersion = "SW_1.0.2.2",
+                    Created = DateTime.Now.AddDays(-10),
+                    Status = DefectStatusEnum.Resolved,
+                    Component = ComponentEnum.Feature,
+                    SoftwareId = 10,
+                    TestId = 9,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                    End = DateTime.Now.AddDays(-9)
+                },
+                new
+                {
+                    Id = 12,
+                    Title = "[AUTH] Password reset link not expiring",
+                    Description = "1. Click on 'Forgot Password'\n2. Provide the email\n3. Check if the password reset link is expiring properly",
+                    ReleaseId = 3,
+                    IsRegression = false,
+                    IsFixed = true,
+                    CompletedReason = DefectCompletedReasonEnum.Fixed,
+                    FixedOnVersion = "SW_1.0.2.3",
+                    Created = DateTime.Now.AddDays(-9),
+                    Status = DefectStatusEnum.Resolved,
+                    Component = ComponentEnum.Authentication,
+                    SoftwareId = 11,
+                    TestId = 10,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                    End = DateTime.Now.AddDays(-8)
+                },
+                new
+                {
+                    Id = 13,
+                    Title = "[DATA] Unable to update records",
+                    Description = "1. Attempt to update a record\n2. Check if the record is successfully updated",
+                    ReleaseId = 3,
+                    IsRegression = false,
+                    IsFixed = true,
+                    CompletedReason = DefectCompletedReasonEnum.Fixed,
+                    FixedOnVersion = "SW_1.0.2.4",
+                    Created = DateTime.Now.AddDays(-8),
+                    Status = DefectStatusEnum.Resolved,
+                    Component = ComponentEnum.Data,
+                    SoftwareId = 12,
+                    TestId = 11,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                    End = DateTime.Now.AddDays(-7)
+                },
+                new
+                {
+                    Id = 14,
+                    Title = "[FEATURE] Missing feature images",
+                    Description = "1. Open the feature details\n2. Check if the images associated with the feature are displayed",
+                    ReleaseId = 3,
+                    IsRegression = false,
+                    IsFixed = true,
+                    CompletedReason = DefectCompletedReasonEnum.Fixed,
+                    FixedOnVersion = "SW_1.0.2.5",
+                    Created = DateTime.Now.AddDays(-7),
+                    Status = DefectStatusEnum.Resolved,
+                    Component = ComponentEnum.Feature,
+                    SoftwareId = 13,
+                    TestId = 12,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                    End = DateTime.Now.AddDays(-6)
+                },
+                new
+                {
+                    Id = 15,
+                    Title = "[DATA] Incorrect data calculations",
+                    Description = "1. Perform data calculations\n2. Check if the calculated data is correct",
+                    ReleaseId = 3,
+                    IsRegression = false,
+                    IsFixed = true,
+                    CompletedReason = DefectCompletedReasonEnum.Fixed,
+                    FixedOnVersion = "SW_1.0.2.6",
+                    Created = DateTime.Now.AddDays(-6),
+                    Status = DefectStatusEnum.Resolved,
+                    Component = ComponentEnum.Data,
+                    SoftwareId = 14,
+                    TestId = 13,
+                    UsersInvolvedEmails = "dzyzusek@gmail.com;wrk.mmarzec@gmail.com;",
+                    End = DateTime.Now.AddDays(-5)
                 }
                 );
 
